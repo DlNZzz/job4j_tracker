c3af21d9e2d0d011e66d18b293d6727a20f34d8d
Dinz <kostan-82@yandex.ru>
2021-03-28 18:15:23 +0300

    9.1 Зависимость от System.out [#496532]

--- src/main/java/ru/job4j/tracker/AllAction.java
+++ src/main/java/ru/job4j/tracker/AllAction.java
@@ -1,6 +1,12 @@
 package ru.job4j.tracker;
 
 public class AllAction implements UserAction {
+    private final Output out;
+
+    public AllAction(Output out) {
+        this.out = out;
+    }
+
     @Override
     public String name() {
         return "Show all items";
@@ -8,7 +14,7 @@ public class AllAction implements UserAction {
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
-        System.out.println("==== Show all items ====");
+        out.println("==== Show all items ====");
         Item[] items = tracker.findAll();
         for (Item item : items) {
             System.out.println(item);
--- src/main/java/ru/job4j/tracker/IdAction.java
+++ src/main/java/ru/job4j/tracker/IdAction.java
@@ -1,6 +1,12 @@
 package ru.job4j.tracker;
 
 public class IdAction implements UserAction {
+    private final Output out;
+
+    public IdAction(Output out) {
+        this.out = out;
+    }
+
     @Override
     public String name() {
         return "Find item by Id";
@@ -8,7 +14,7 @@ public class IdAction implements UserAction {
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
-        System.out.println("==== Find item by Id ====");
+        out.println("==== Find item by Id ====");
         int id = input.askInt("Enter ID: ");
         Item item = tracker.findById(id);
         if (item == null) {
