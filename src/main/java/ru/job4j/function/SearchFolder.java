05edbf06b8a426f962089a5c3caacdf37b30d8fe
Dinz <kostan-82@yandex.ru>
2021-04-16 23:21:43 +0300

    1.3. Встроенные функциональные интерфейсы. [#496339]

---
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -0,0 +1,26 @@
+package ru.job4j.function;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SearchFolder {
+    public boolean check(Predicate<String> pred, String s) {
+        return pred.test(s);
+    }
+
+    public static List<Folder> filter(List<Folder> list, Predicate<Folder> pred) {
+        List<Folder> rsl = new ArrayList<>();
+        SearchFolder searchFolder = new SearchFolder();
+        for (Folder f : list) {
+            if (searchFolder.check(str -> str.contains("bug"), f.getName()) && pred.test(f)) {
+                rsl.add(f);
+            }
+            if (searchFolder.check(i -> Integer.parseInt(i) > 100,
+                    String.valueOf(f.getSize())) && pred.test(f)) {
+                rsl.add(f);
+            }
+        }
+        return rsl;
+    }
+}
