f0fea7f0ec807e27b192dcb9153a6c7d62246920
Dinz <kostan-82@yandex.ru>
2021-04-21 13:15:45 +0300

    5. Подсчет функции в диапазоне. [#496331]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -7,15 +7,7 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String o1, String o2) {
         String[] o1Array = o1.split("/");
         String[] o2Array = o2.split("/");
-        int o1Length = o1.split("/").length;
-        int o2Length = o2.split("/").length;
-        if (o1Array[0].compareTo(o2Array[0]) == 0) {
-            for (int i = 1; i < Math.min(o1Length, o2Length); i++) {
-                if (o1Array[i].compareTo(o2Array[i]) != 0) {
-                    return o1Array[i].compareTo(o2Array[i]);
-                }
-            }
-        }
-        return o1Length > o2Length ? 1 : -1;
+        int rsl = o2Array[0].compareTo(o1Array[0]);
+        return rsl == 0 ? o1.compareTo(o2) : rsl;
     }
 }
