e7911b25d499118963b9054d4d2ea8d3acfadf71
Dinz <kostan-82@yandex.ru>
2021-04-23 14:08:09 +0300

    0. Stream API [#496345]

---
+++ src/main/java/ru/job4j/lambda/StreamUsage.java
@@ -0,0 +1,16 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamUsage {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(-2);
+        list.add(3);
+        List<Integer> list2 = list.stream().filter(task -> task > 0).collect(Collectors.toList());
+    }
+}
