5cd1cf437338d6e1947973c26400e657a394b78d
Dinz <kostan-82@yandex.ru>
2021-05-16 22:32:07 +0300

    EasyStream [#401629]

--- src/main/java/ru/job4j/stream/EasyStream.java
+++ src/main/java/ru/job4j/stream/EasyStream.java
@@ -3,23 +3,33 @@ package ru.job4j.stream;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Stream;
-import java.util.stream.Stream.Builder;
+import java.util.stream.Collectors;
 
 public class EasyStream {
+
+    private List<Integer> list;
+
+    private EasyStream(List<Integer> list) {
+        this.list = list;
+    }
+
     public static EasyStream of(List<Integer> source) {
-        throw new UnsupportedOperationException();
+        return new EasyStream(source);
     }
 
     public EasyStream map(Function<Integer, Integer> fun) {
-        throw new UnsupportedOperationException();
+        return new EasyStream(list.stream()
+                .map(fun)
+                .collect(Collectors.toList()));
     }
 
     public EasyStream filter(Predicate<Integer> fun) {
-        throw new UnsupportedOperationException();
+        return new EasyStream(list.stream()
+                .filter(fun)
+                .collect(Collectors.toList()));
     }
 
     public List<Integer> collect() {
-        throw new UnsupportedOperationException();
+        return list;
     }
 }
--- src/test/java/ru/job4j/stream/EasyStreamTest.java
+++ src/test/java/ru/job4j/stream/EasyStreamTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import java.util.List;
 
 public class EasyStreamTest {
-/*
     @Test
     public void whenEmptySource() {
         var rsl = EasyStream.of(List.of())
@@ -63,6 +62,4 @@ public class EasyStreamTest {
                 .collect();
         assertThat(rsl, is(List.of(2, 3, 4)));
     }
-
- */
 }
